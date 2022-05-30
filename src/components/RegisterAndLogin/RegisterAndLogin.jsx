302e16ee6038071ced5cdc74a414e5ca4c590f9f
naimelh <naimelhammouchi@icloud.com>
2022-05-30 14:04:55 +0200

    setup login

--- src/components/RegisterAndLogin/RegisterAndLogin.jsx
+++ src/components/RegisterAndLogin/RegisterAndLogin.jsx
@@ -4,10 +4,74 @@ import "./registerandlogin.scss";
 
 const RegisterAndLogin = () => {
   return (
-    <div>
-      <Navbar />
-      RegisterAndLogin
-    </div>
+    <>
+      <section class="hero is-primary is-fullheight">
+        <div class="hero-body">
+          <div class="container">
+            <div class="container has-text-centered">
+              <div class="column is-8 is-offset-2">
+                <h3 class="title has-text-white">Login</h3>
+                <hr class="login-hr" />
+                <p class="subtitle has-text-white">Please login !</p>
+                <div class="box">
+                  <div class="box">
+                    <img src="" alt="Photo of the logo" />
+                  </div>
+                </div>
+                <form action="" class="box">
+                  <div class="field">
+                    <label for="" class="label">
+                      Email
+                    </label>
+                    <div class="control has-icons-left">
+                      <input
+                        type="email"
+                        placeholder="e.g. bobsmith@gmail.com"
+                        class="input"
+                        required
+                      />
+                      <span class="icon is-small is-left">
+                        <i class="fa fa-envelope"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div class="field">
+                    <label for="" class="label">
+                      Password
+                    </label>
+                    <div class="control has-icons-left">
+                      <input
+                        type="password"
+                        placeholder="*******"
+                        class="input"
+                        required
+                      />
+                      <span class="icon is-small is-left">
+                        <i class="fa fa-lock"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div class="field">
+                    <label for="" class="checkbox">
+                      <input type="checkbox" />
+                      Remember me
+                    </label>
+                  </div>
+                  <div class="field">
+                    <button class="button is-success">Login</button>
+                  </div>
+                </form>
+                <p class="has-text-grey">
+                  <a href="">Sign Up</a>
+                  <a href="">Forgot Password</a>
+                  <a href="">Need Help?</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
