138e7773288641634ec5f0fa02968a2b57cc7aa9
AronH99 <aronheremans@gmail.com>
2022-05-31 22:09:58 +0200

    finished portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <div className="navbar-item">
               <img src="/src/images/icoon.png" alt="logo" id="logoImg" />
               <p className="title" id="navTitle">
-                Coin Getto
+                CoinGetto
               </p>
             </div>
 
