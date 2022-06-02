81bd113bff6bf0c015b06abae4b16e9132bbe77c
agismero <91531154+agismero@users.noreply.github.com>
2022-06-02 09:52:33 +0200

    added detail component

---
+++ src/components/Detail/Detail.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+import Charts from "../Charts/Charts";
+
+const Detail = () => {
+  return (
+    <>
+      <Navbar />
+      <Charts />
+    </>
+  );
+};
+
+export default Detail;
